65034c1abff61989fa8cd5fec384fdad3c2e57e4
MCLEANS <jackagwaya@gmail.com>
2020-09-22 20:15:17 +0300

    added WIFI and HTTP libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <Arduino.h>
 
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+//Your Domain name with URL path or IP address with path
+const char* serverName = "http://pid-api.herokuapp.com/post/data";
+
+
+
 void setup() {
   
 }
