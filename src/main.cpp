31c2e179f8ead8b8b33a39600d96186ebfe96aa2
MCLEANS <jackagwaya@gmail.com>
2020-09-22 20:19:27 +0300

    Implemented post method

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,45 @@ const char* serverName = "http://pid-api.herokuapp.com/post/data";
 
 
 
+
+
 void setup() {
+
+   Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
   
 }
 
 void loop() {
+
+  //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverName);
+
+      // Specify content-type header
+      http.addHeader("Content-Type", "application/json");
+      int httpResponseCode = http.POST("{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}");
+     
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+        
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
 
 }
